fbb8842f2e8ba14e6bb925c4b19a3e327415a738
andy <cd_wjp@163.com>
2022-08-23 18:20:21 +0800

    add Constants

---
+++ backend/src/main/java/ai/basic/x1/util/Constants.java
@@ -0,0 +1,8 @@
+package ai.basic.x1.util;
+
+public interface Constants {
+    String X_REAL_IP = "X-Real-Ip";
+    String X_HOST = "X-Host";
+    String X_UA = "X-User-Agent";
+    String X_FORWARDED_FOR = "X-Forwarded-For";
+}
