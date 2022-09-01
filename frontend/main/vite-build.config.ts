7d7233c23ae31ee82ef2a5e76e1219db75cc1edc
liujian <liujian@basicfinder.com>
2022-09-01 13:02:03 +0800

    fix: build config

--- frontend/main/vite-build.config.ts
+++ frontend/main/vite-build.config.ts
@@ -2,7 +2,7 @@ import { mergeConfig } from 'vite';
 import baseConfig from './vite.config';
 
 export default mergeConfig(baseConfig, {
-  base: '/pc-tool/',
+  base: '/main/',
   build: {
     outDir: '../dist/main',
   },
