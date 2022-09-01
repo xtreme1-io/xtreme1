4d7d128e77a09bcd09a517aef9b55be1ae074427
liujian <liujian@basicfinder.com>
2022-09-01 13:02:46 +0800

    fix: build config

--- frontend/main/vite-build.config.ts
+++ frontend/main/vite-build.config.ts
@@ -2,7 +2,6 @@ import { mergeConfig } from 'vite';
 import baseConfig from './vite.config';
 
 export default mergeConfig(baseConfig, {
-  base: '/main/',
   build: {
     outDir: '../dist/main',
   },
