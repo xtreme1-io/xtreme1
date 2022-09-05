b8f2bef193584a63ab637c9b9b2e6f84204dd948
Jagger Wang <jaggerwang@gmail.com>
2022-09-05 14:05:58 +0800

    Remove model code

--- .ops/build_and_push_images_to_docker_hub.sh
+++ .ops/build_and_push_images_to_docker_hub.sh
@@ -4,10 +4,7 @@
 
 BACKEND_IMAGE=basicai/x1-community-backend
 FRONTEND_IMAGE=basicai/x1-community-frontend
-IMAGE_OBJECT_DETECTION_IMAGE=basicai/x1-community-image-object-detection
 
 cd ../backend && docker build -t $BACKEND_IMAGE . && docker push $BACKEND_IMAGE
 
 cd ../frontend && docker build -t $FRONTEND_IMAGE . && docker push $FRONTEND_IMAGE
-
-cd ../model/image-object-detection && docker build -t $IMAGE_OBJECT_DETECTION_IMAGE . && docker push $IMAGE_OBJECT_DETECTION_IMAGE
