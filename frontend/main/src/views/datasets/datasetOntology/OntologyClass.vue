99ac94d874fa78cadc4254cf1dacb5331adacac5
Ruovan <2454234258@qq.com>
2022-12-08 22:40:33 +0800

    feat: copy ontology

--- frontend/main/src/views/datasets/datasetOntology/OntologyClass.vue
+++ frontend/main/src/views/datasets/datasetOntology/OntologyClass.vue
@@ -6,7 +6,7 @@
         <VirtualTab :list="tabListOntology" />
       </div>
       <div class="btn">
-        <HeaderDropdown :activeTab="activeTab" :datasetType="datasetType" />
+        <HeaderDropdown :datasetId="datasetId" @fetchList="handleRefresh" />
       </div>
       <div class="mb-15px">
         <Action
