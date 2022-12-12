bdf50c4dc4ff290bb0c073651aa467e0b6fcaf32
butterflyzh <butterflyzhu@126.com>
2022-12-12 17:56:38 +0800

    fix dataset classification option search

--- backend/src/main/java/ai/basic/x1/usecase/DataClassificationOptionUseCase.java
+++ backend/src/main/java/ai/basic/x1/usecase/DataClassificationOptionUseCase.java
@@ -1,6 +1,8 @@
 package ai.basic.x1.usecase;
 
+import ai.basic.x1.adapter.port.dao.DataAnnotationObjectDAO;
 import ai.basic.x1.adapter.port.dao.DataClassificationOptionDAO;
+import ai.basic.x1.adapter.port.dao.mybatis.model.DataAnnotationObject;
 import ai.basic.x1.adapter.port.dao.mybatis.model.DataClassificationOption;
 import ai.basic.x1.entity.DataAnnotationClassificationBO;
 import ai.basic.x1.entity.DataClassificationOptionBO;
@@ -10,13 +12,16 @@ import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DataClassificationOptionUseCase {
 
     @Autowired
     private DataClassificationOptionDAO dataClassificationOptionDAO;
+
+    @Autowired
+    private DataAnnotationObjectDAO dataAnnotationObjectDAO;
 
 
     public void saveBatch(List<DataAnnotationClassificationBO> dataAnnotations) {
@@ -44,16 +49,62 @@ public class DataClassificationOptionUseCase {
         var page = new com.baomidou.mybatisplus.extension.plugins.pagination.Page<DataClassificationOption>(pageNo, pageSize);
         var records = dataClassificationOptionDAO.getBaseMapper()
                 .statisticsDataByOption(page, datasetId).getRecords();
+
+        setOptionPath(records);
         return DefaultConverter.convert(records, DataClassificationOptionBO.class);
+    }
+
+    private void setOptionPath(List<DataClassificationOption> options) {
+        var attributeIds = options.stream().map(DataClassificationOption::getAttributeId).collect(Collectors.toSet());
+        var optionNames = options.stream().map(DataClassificationOption::getOptionName).collect(Collectors.toSet());
+        if (CollUtil.isEmpty(attributeIds) || CollUtil.isEmpty(optionNames)) {
+            return;
+        }
+        var optionMap = dataClassificationOptionDAO.list(new LambdaQueryWrapper<DataClassificationOption>()
+                .in(DataClassificationOption::getAttributeId, attributeIds)
+                .in(DataClassificationOption::getOptionName, optionNames)
+        ).stream().collect(Collectors.toMap(e -> String.format("%s-%s", e.getAttributeId(),
+                e.getOptionName()), t -> t, (oldV, newV) -> newV));
+
+        options.forEach(op -> {
+            var key = String.format("%s-%s", op.getAttributeId(), op.getOptionName());
+            op.setOptionPath(optionMap.getOrDefault(key, new DataClassificationOption()).getOptionPath());
+        });
     }
 
     public List<DataClassificationOptionBO> findByClassIds(List<Long> classIds) {
         if (CollUtil.isEmpty(classIds)) {
             return List.of();
         }
-        var options = dataClassificationOptionDAO.getBaseMapper()
-                .findByClassIds(classIds);
-        return DefaultConverter.convert(options, DataClassificationOptionBO.class);
+        var dataIds = dataAnnotationObjectDAO.list(new LambdaQueryWrapper<DataAnnotationObject>()
+                        .in(DataAnnotationObject::getClassId, classIds))
+                .stream().map(DataAnnotationObject::getDataId).collect(Collectors.toSet());
+        if (CollUtil.isEmpty(dataIds)) {
+            return List.of();
+        }
+
+        var options = dataClassificationOptionDAO.list(new LambdaQueryWrapper<DataClassificationOption>()
+                .in(DataClassificationOption::getDataId, dataIds));
+        return groupCountInMemory(options);
+    }
+
+    private List<DataClassificationOptionBO> groupCountInMemory(List<DataClassificationOption> options) {
+        Map<String, DataClassificationOptionBO> statisticsMap = new HashMap<>();
+        for (var option : options) {
+            var key = String.format("%s-%s", option.getAttributeId(), option.getOptionName());
+            var optionBo = statisticsMap.get(key);
+            if (optionBo != null) {
+                optionBo.setDataAmount(optionBo.getDataAmount() + 1L);
+            } else {
+                optionBo = DataClassificationOptionBO.builder().build();
+                optionBo.setAttributeId(option.getAttributeId());
+                optionBo.setOptionName(option.getOptionName());
+                optionBo.setOptionPath(option.getOptionPath());
+                optionBo.setDataAmount(1L);
+                statisticsMap.put(key, optionBo);
+            }
+        }
+        return new ArrayList<>(statisticsMap.values());
     }
 
 }
