116983aa3e27eede85077aaeeef5df2ba02a87ca
guhao <guhao@basicfinder.com>
2022-12-19 18:40:16 +0800

    fix: fix bug

--- frontend/main/src/views/ontology/scenario/index.vue
+++ frontend/main/src/views/ontology/scenario/index.vue
@@ -197,7 +197,7 @@
     }
     if (info.value.type === datasetTypeEnum.LIDAR_FUSION) {
       const obj2dMap = {};
-      res.list.forEach((item: any) => {
+      res.list?.forEach((item: any) => {
         const type = item.classAttributes.type || item.classAttributes.objType;
         const info = item.classAttributes;
         if (['2D_RECT', '2D_BOX', 'rect', 'box2d'].includes(type)) {
@@ -213,7 +213,7 @@
       object2D.value = obj2dMap;
       list.value = _list;
     } else {
-      list.value = res.list;
+      list.value = res.list || [];
     }
   };
 
