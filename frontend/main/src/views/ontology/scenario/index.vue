ff910f1efb5569987176057f43024f5f397872fc
刘剑 <>
2022-12-19 16:44:32 +0800

    fix: some style

--- frontend/main/src/views/ontology/scenario/index.vue
+++ frontend/main/src/views/ontology/scenario/index.vue
@@ -23,8 +23,10 @@
           @change="handleChange"
         >
           <Select.Option v-for="item in options" :key="item.id" :value="item.id">
-            <img :src="toolTypeImg[item.toolType]" alt="" />
-            {{ item.name }}
+            <div class="inline-flex items-center">
+              <img class="mr-1" width="14" height="14" :src="toolTypeImg[item.toolType]" alt="" />
+              {{ item.name }}
+            </div>
           </Select.Option>
         </Select>
         <Button class="ml-20px" type="default" @click="openModal">Export Result</Button>
