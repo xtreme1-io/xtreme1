78a4252b13f8e5db8d794cbc9f2f0efebc6019fb
刘剑 <>
2023-02-23 10:14:59 +0800

    fix: remove email validate

--- frontend/main/src/utils/business/regexp.ts
+++ frontend/main/src/utils/business/regexp.ts
@@ -1,1 +1,2 @@
-export const emailReg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+export const emailReg =
+  /^[A-Za-z0-9\u4e00-\u9fa5_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
