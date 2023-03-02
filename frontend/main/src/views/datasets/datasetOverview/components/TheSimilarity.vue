2f99505dd9318125dd5171ba44fd57714a1cf049
wutianchi <wutianchi@basicfinder.com>
2023-03-02 17:03:57 +0800

    feat:  set Similarity Map height:width =1:2

--- frontend/main/src/views/datasets/datasetOverview/components/TheSimilarity.vue
+++ frontend/main/src/views/datasets/datasetOverview/components/TheSimilarity.vue
@@ -75,7 +75,7 @@
         </div>
       </div>
       <div class="chartContainer">
-        <div ref="plotRef" class="chartContainer__box"></div>
+        <div class="container"> <div ref="plotRef" class="chartContainer__box"></div> </div>
       </div>
     </template>
   </div>
@@ -331,8 +331,21 @@
 </script>
 <style lang="less" scoped>
   @import './index.less';
-  .chartContainer__box {
-    width: 90%;
+
+  .container {
+    width: 100%;
+    height: 0;
+    position: relative;
+    padding-top: 50%;
+    .chartContainer__box {
+      display: block;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      padding: 0 20px;
+    }
   }
   .brush_container {
     display: flex;
