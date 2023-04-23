916aa8c9be637002185ae216dd18224f99c9b054
raincolor <1037428213@qq.com>
2023-04-23 10:28:32 +0800

    fix Issue of  loading in   high resolution
    Signed-off-by: raincolor <1037428213@qq.com>

--- frontend/main/src/utils/business/scrollListener.ts
+++ frontend/main/src/utils/business/scrollListener.ts
@@ -1,6 +1,21 @@
 import { unref, Ref } from 'vue';
-export const handleScroll = (scrollRef: Ref, callback: Function) => {
+export const handleScroll = (scrollRef: Ref, callback: Function, canload?) => {
   const dom = unref(scrollRef)?.getScrollWrap();
+  // console.log('scroll:', unref(scrollRef));
+  let timerCount = 0;
+  const load = () => {
+    setTimeout(() => {
+      // console.log(dom?.childNodes[0].clientHeight, dom?.clientHeight, canload);
+      if (dom?.childNodes[0].clientHeight < dom?.clientHeight && canload) {
+        callback();
+      }
+      if (canload && timerCount < 5) {
+        timerCount += 1;
+        load();
+      }
+    }, 800);
+  };
+  load();
   let timer;
   dom?.addEventListener('scroll', function () {
     clearTimeout(timer);
@@ -10,4 +25,18 @@ export const handleScroll = (scrollRef: Ref, callback: Function) => {
       }
     }, 500);
   });
+};
+
+export const initializeData = (scrollRef: Ref, callback: Function, canload?) => {
+  const dom = unref(scrollRef)?.getScrollWrap();
+  const load = () => {
+    setTimeout(() => {
+      // console.log(dom?.childNodes[0].clientHeight, dom?.clientHeight, canload);
+      if (dom?.childNodes[0].clientHeight < dom?.clientHeight && canload) {
+        callback();
+        load();
+      }
+    }, 300);
+  };
+  load();
 };
