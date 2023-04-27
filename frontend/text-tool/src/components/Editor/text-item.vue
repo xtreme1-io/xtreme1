97c904ec25284a45d5d94f58e3da434d79987f5f
raincolor <1037428213@qq.com>
2023-04-27 17:36:03 +0800

    feat: text-tool updated

--- frontend/text-tool/src/components/Editor/text-item.vue
+++ frontend/text-tool/src/components/Editor/text-item.vue
@@ -1,62 +1,128 @@
 <template>
-  <div class="flex" v-if="item?.role === 'prompter'">
-    <div class="user"> A </div>
-    <div class="prompter">
-      <div>
-        {{ item?.text }}
-      </div>
-      <div class="thumbs">
-        <SvgIcon
-          @click="item.direction = ''"
-          v-if="item.direction === 'up'"
-          name="had-thumbs-up"
-        />
-        <SvgIcon @click="item.direction = 'up'" v-else name="thumbs-up" />
+    <div class="text" v-if="item?.role === 'prompter'">
+        <div class="user"> A </div>
+        <div class="prompter">
+            <div>
+                {{ item?.text }}
+            </div>
+            <div class="thumbs">
+                <img
+                    :src="thumbsUp"
+                    v-if="item.direction !== 'up'"
+                    @click="item.direction = 'up'"
+                />
+                <img
+                    v-if="item.direction === 'up'"
+                    :src="hadThumbsUp"
+                    @click="item.direction = ''"
+                />
 
-        <SvgIcon
-          @click="item.direction = ''"
-          v-if="item.direction === 'down'"
-          name="had-thumbs-down"
-        />
-        <SvgIcon @click="item.direction = 'down'" v-else name="thumbs-down" />
-      </div>
+                <img
+                    :src="thumbsDown"
+                    v-if="item.direction !== 'down'"
+                    @click="item.direction = 'down'"
+                />
+                <img
+                    v-if="item.direction === 'down'"
+                    :src="hadThumbsDown"
+                    @click="item.direction = ''"
+                    name="hasThumbsDown"
+                />
+            </div>
+        </div>
     </div>
-  </div>
-  <div style="justify-content: end" class="flex" v-if="item?.role === 'assistant'">
-    <div class="assistant">
-      <div>
-        {{ item?.text }}
-      </div>
+    <div style="justify-content: end" class="flex text" v-if="item?.role === 'assistant'">
+        <div class="assistant">
+            <div>
+                {{ item?.text }}
+            </div>
 
-      <div class="thumbs">
-        <SvgIcon
-          @click="item.direction = ''"
-          v-if="item.direction === 'up'"
-          name="had-thumbs-up"
-        />
-        <SvgIcon @click="item.direction = 'up'" v-else name="thumbs-up" />
+            <div class="thumbs">
+                <img
+                    :src="thumbsUp"
+                    v-if="item.direction !== 'up'"
+                    @click="item.direction = 'up'"
+                />
+                <img
+                    v-if="item.direction === 'up'"
+                    :src="hadThumbsUp"
+                    @click="item.direction = ''"
+                />
 
-        <SvgIcon
-          @click="item.direction = ''"
-          v-if="item.direction === 'down'"
-          name="had-thumbs-down"
-        />
-        <SvgIcon @click="item.direction = 'down'" v-else name="thumbs-down" />
-      </div>
+                <img
+                    :src="thumbsDown"
+                    v-if="item.direction !== 'down'"
+                    @click="item.direction = 'down'"
+                />
+                <img
+                    v-if="item.direction === 'down'"
+                    :src="hadThumbsDown"
+                    @click="item.direction = ''"
+                    name="hasThumbsDown"
+                />
+            </div>
+        </div>
+        <div class="user"> B </div>
     </div>
-    <div class="user"> B </div>
-  </div>
 </template>
 
 <script setup lang="ts">
-  import { ITextItem } from 'pc-editor';
+    import { ITextItem } from 'pc-editor';
+    import thumbsUp from '/src/assets/img/thumbs-up.png';
+    import hadThumbsUp from '/src/assets/img/had-thumbs-up.png';
+    import thumbsDown from '/src/assets/img/thumbs-down.png';
+    import hadThumbsDown from '/src/assets/img/had-thumbs-down.png';
 
-  interface IProps {
-    item: ITextItem
-  }
+    interface IProps {
+        item: ITextItem;
+    }
 
-  const props = defineProps<IProps>();
-  function onDirection(type: 'up' | 'down') {}
+    const props = defineProps<IProps>();
+    function onDirection(type: 'up' | 'down') {}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+    .text {
+        width: calc(100% - 450px);
+        display: flex;
+    }
+    .thumbs {
+        text-align: right;
+        padding-top: 10px;
+        img {
+            margin: 0 6px;
+            cursor: pointer;
+        }
+    }
+    .prompter {
+        // word-break: break-all;
+        max-width: calc(100% - 150px);
+        color: #fff;
+        font-size: 16px;
+        line-height: 26px;
+        padding: 16px;
+        background: #555b76;
+        border-radius: 0px 24px 24px 24px;
+        margin-bottom: 20px;
+    }
+    .assistant {
+        max-width: calc(100% - 150px);
+        color: #fff;
+        font-size: 16px;
+        line-height: 26px;
+        padding: 16px;
+        background: #5e80a9;
+        border-radius: 24px 0px 24px 24px;
+        margin-bottom: 20px;
+    }
+    .user {
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        border: 1px solid #aaa;
+        text-align: center;
+        line-height: 30px;
+        margin: 0 10px;
+        background: #fff;
+    }
+</style>
--- frontend/text-tool/src/components/Editor/text-main.vue
+++ frontend/text-tool/src/components/Editor/text-main.vue
@@ -9,7 +9,7 @@
     import { Event, ITextItem } from 'pc-editor';
     import { useInjectEditor } from '../../state';
 
-    import TextItem from './text-item.vue'
+    import TextItem from './text-item.vue';
 
     const editor = useInjectEditor();
     editor.addEventListener(Event.ANNOTATE_LOADED, () => {
@@ -22,58 +22,15 @@
         dataList.value = Array.from(dataMap.values());
         console.log('==============>', dataList);
     }
-
 </script>
 
 <style lang="less">
- .pc-editor-tool {
+    .pc-editor-tool {
         padding: 20px 10px;
         display: flex;
         flex-direction: column;
         align-items: center;
         position: relative;
-
-        & > div {
-            width: 100%;
-            .thumbs {
-                text-align: right;
-                padding-top: 10px;
-                svg {
-                    margin: 0 6px;
-                    cursor: pointer;
-                }
-            }
-            .prompter {
-                // word-break: break-all;
-                max-width: calc(100% - 150px);
-                color: #fff;
-                font-size: 16px;
-                line-height: 26px;
-                padding: 16px;
-                background: #555b76;
-                border-radius: 0px 24px 24px 24px;
-                margin-bottom: 20px;
-            }
-            .assistant {
-                max-width: calc(100% - 150px);
-                color: #fff;
-                font-size: 16px;
-                line-height: 26px;
-                padding: 16px;
-                background: #5e80a9;
-                border-radius: 24px 0px 24px 24px;
-                margin-bottom: 20px;
-            }
-            .user {
-                width: 30px;
-                height: 30px;
-                border-radius: 50%;
-                border: 1px solid #aaa;
-                text-align: center;
-                line-height: 30px;
-                margin: 0 10px;
-                background: #fff;
-            }
-        }
+        width: calc(100% - 350px);
     }
 </style>
