38d664f74dfb5d75ba1c7eb9f278d6d861ae2a8c
guhao <guhao@basicfinder.com>
2023-10-08 15:22:44 +0800

    fix: token bug

--- frontend/image-tool/src/business/chengdu/hook/useToken.ts
+++ frontend/image-tool/src/business/chengdu/hook/useToken.ts
@@ -3,15 +3,17 @@ import Cookies from 'js-cookie';
 window.Cookies = Cookies;
 
 let hostname = document.location.hostname || document.location.host;
-let dot = hostname.indexOf('.');
-let parentHost = isIp(hostname) ? hostname : hostname.substring(dot + 1);
+// let dot = hostname.indexOf('.');
+// let parentHost = isIp(hostname) ? hostname : hostname.substring(dot + 1);
 
 export default function useToken() {
-    let token = Cookies.get(`${parentHost} token`);
+    let token = Cookies.get(`${hostname} token`);
     token = token ? `Bearer ${token}` : '';
     return token;
 }
 
-function isIp(str:string = ''){
-    return /((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))/.test(str);
+function isIp(str: string = '') {
+    return /((25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))/.test(
+        str,
+    );
 }
