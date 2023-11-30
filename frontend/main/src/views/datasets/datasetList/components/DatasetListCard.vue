50cfdf5136508338ad20e08ac9218043a23fc42f
Ruovan <2454234258@qq.com>
2023-11-30 10:11:08 +0800

    fix: fix image dataset display

--- frontend/main/src/views/datasets/datasetList/components/DatasetListCard.vue
+++ frontend/main/src/views/datasets/datasetList/components/DatasetListCard.vue
@@ -185,7 +185,7 @@
 
   const getImgUrl = (index) => {
     const files = props.data.datas || [];
-    const file = files[index]?.content[0]?.file;
+    const file = files[index]?.content[0]?.file ?? files[index]?.content[0]?.files?.[0]?.file;
     return file?.mediumThumbnail?.url || file?.url || placeImg;
   };
 
