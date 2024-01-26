c22697a7d2411b7af9edd5f47cffb6731004a782
fanyinbo <1553199396@qq.com>
2024-01-26 16:38:43 +0800

    Fix the same data exists in multiple consecutive frames, The data cannot be uploaded.
    Signed-off-by: fanyinbo <1553199396@qq.com>

--- backend/src/main/java/ai/basic/x1/usecase/UploadDataUseCase.java
+++ backend/src/main/java/ai/basic/x1/usecase/UploadDataUseCase.java
@@ -304,6 +304,7 @@ public class UploadDataUseCase {
         var rootPath = String.format("%s/%s", userId, datasetId);
         var errorBuilder = new StringBuilder();
         var dataInfoBOBuilder = DataInfoBO.builder().datasetId(datasetId)
+                .parentId(Constants.DEFAULT_PARENT_ID)
                 .type(ItemTypeEnum.SINGLE_DATA)
                 .status(DataStatusEnum.VALID)
                 .annotationStatus(DataAnnotationStatusEnum.NOT_ANNOTATED)
@@ -349,7 +350,7 @@ public class UploadDataUseCase {
                         dataInfoBOList.add(dataInfoBO);
                     });
                     if (CollectionUtil.isNotEmpty(dataInfoBOList)) {
-                        insertBatch(dataInfoBOList, datasetId, errorBuilder);
+                        insertBatch(dataInfoBOList, datasetId, errorBuilder, Constants.DEFAULT_PARENT_ID);
                     }
                 } catch (Exception e) {
                     log.error("Handle data error", e);
@@ -498,7 +499,7 @@ public class UploadDataUseCase {
                         }
                     });
                     if (CollectionUtil.isNotEmpty(dataInfoBOList)) {
-                        var resDataInfoList = this.insertBatch(dataInfoBOList, datasetId, errorBuilder);
+                        var resDataInfoList = this.insertBatch(dataInfoBOList, datasetId, errorBuilder, sceneId);
                         this.saveBatchDataResult(resDataInfoList, dataAnnotationObjectBOList);
                     }
                 } catch (Exception e) {
@@ -1045,9 +1046,9 @@ public class UploadDataUseCase {
      *
      * @param dataInfoBOList Collection of data details
      */
-    public List<DataInfoBO> insertBatch(List<DataInfoBO> dataInfoBOList, Long datasetId, StringBuilder errorBuilder) {
+    public List<DataInfoBO> insertBatch(List<DataInfoBO> dataInfoBOList, Long datasetId, StringBuilder errorBuilder, Long parentId) {
         var names = dataInfoBOList.stream().map(DataInfoBO::getName).collect(Collectors.toList());
-        var existDataInfoList = this.findByNames(datasetId, names);
+        var existDataInfoList = this.findByNames(datasetId, parentId, names);
         if (CollUtil.isNotEmpty(existDataInfoList)) {
             var existNames = existDataInfoList.stream().map(DataInfoBO::getName).collect(Collectors.toList());
             dataInfoBOList = dataInfoBOList.stream().filter(dataInfoBO -> !existNames.contains(dataInfoBO.getName())).collect(Collectors.toList());
@@ -1071,10 +1072,11 @@ public class UploadDataUseCase {
         }
     }
 
-    private List<DataInfoBO> findByNames(Long datasetId, List<String> names) {
+    private List<DataInfoBO> findByNames(Long datasetId, Long parentId, List<String> names) {
         var dataInfoLambdaQueryWrapper = Wrappers.lambdaQuery(DataInfo.class);
         dataInfoLambdaQueryWrapper.eq(DataInfo::getDatasetId, datasetId);
         dataInfoLambdaQueryWrapper.in(DataInfo::getName, names);
+        dataInfoLambdaQueryWrapper.eq(DataInfo::getParentId, parentId);
         return DefaultConverter.convert(dataInfoDAO.list(dataInfoLambdaQueryWrapper), DataInfoBO.class);
     }
 
